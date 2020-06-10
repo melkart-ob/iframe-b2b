bbf7d9f1707a052c84dad98c92582de2b1ac52cb
Gabriel <gabriel.enr1998@gmail.com>
2020-06-10 13:34:59 -0400

    configuracion libreria iframe

---
+++ lib/index.d.ts
@@ -0,0 +1,2 @@
+export declare const getSrcIframe: (name: string) => string;
+export declare const initIframe: (id: string) => void;
---
+++ src/__tests__/test.ts
@@ -0,0 +1,4 @@
+import { initIframe } from '../index';
+test('Iframe test', () => {
+    expect(initIframe('iframe-div'))
+});
